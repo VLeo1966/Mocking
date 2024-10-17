313e2b39b71249fc6e0747db3cbe89b29376fe9c
VLeo1966 <148269734+VLeo1966@users.noreply.github.com>
2024-10-17 16:56:04 +0300

    lesson create

--- main.py
+++ main.py
@@ -7,3 +7,11 @@ def get_weather(api_key, city):
         return response.json()
     else:
         return None
+
+def get_github_user(username):
+    url = f'https://api.github.com/users/{username}'
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
